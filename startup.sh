13769eef57b01237c25b2986e0fbd01cae5288e9
Quynh Nguyen <qnguyen@linagora.com>
2016-08-10 17:39:18 +0700

    add altnamespace = yes so logged in user can create new mailbox

--- startup.sh
+++ startup.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
+chmod 755 /var/run/saslauthd 
+
 /etc/init.d/saslauthd start
 
+#echo 'imapuser'|saslpasswd2 -u test -p -c password
 echo 'cyrus'|saslpasswd2 -u test -p -c cyrus
 echo 'bob'|saslpasswd2 -u test -c bob -p
 echo 'alice'|saslpasswd2 -u test -c alice -p
 
-chown -R cyrus:mail /var/spool/imap /var/imap
+chown cyrus /etc/sasldb2*
+
+#chown -R cyrus:mail /var/spool/imap /var/imap
 
 /usr/local/cyrus/bin/master
